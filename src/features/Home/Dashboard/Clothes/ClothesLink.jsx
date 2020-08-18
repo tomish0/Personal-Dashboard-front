ce83d7f818f492e75116f0035a24320c64824057
Tom Isherwood <tomish0@outlook.com>
2020-08-18 16:41:40 +0100

    completed favourite warmer

--- src/features/Home/Dashboard/Clothes/ClothesLink.jsx
+++ src/features/Home/Dashboard/Clothes/ClothesLink.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
+import { PieChart } from "react-minimal-pie-chart";
 
-function ClothesLink(props) {
-    return (
-        <div>
-            ClothesLink
-        </div>
-    );
+import data from "../../../../Assets/clothesPercentages.json";
+
+function ClothesLink() {
+  return (
+    <div>
+      <PieChart
+        data={[
+          { title: 'jumper', value: data.jumper.percentage, color: "#E38627" },
+          { title: 'hoodie', value: data.hoodie.percentage, color: "#000000" },
+          { title: 'jacket', value: data.jacket.percentage, color: "#222299" },
+          { title: 'sweater', value: data.sweater.percentage, color: "#123456" },
+          { title: 'blazer', value: data.blazer.percentage, color: "#999999" },
+          { title: 'raincoat', value: data.raincoat.percentage, color: "#E55533" },
+
+        ]}
+        label={({ dataEntry }) => dataEntry.title}
+      />
+      ;
+    </div>
+  );
 }
 
 export default ClothesLink;
